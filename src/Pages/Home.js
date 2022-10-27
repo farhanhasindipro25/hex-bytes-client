1850603860f4cb9d6c054757eebe42daf934c497
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-10-28 00:00:26 +0600

    final commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h3 className="text-dark text-center fw-bold mt-5">Welcome Aboard</h3>
+    </div>
+  );
+};
+
+export default Home;
